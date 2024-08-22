9ca2c9f65f224ecd3537ac6ae5135e8b4acb3475
QuangDo <do.quang039@gmail.com>
2024-08-22 14:52:48 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { RequestOptions } from 'http'
+
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req: RequestOptions, res: any) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
