06711e9807dbf3f066f18b1f901ef95ab4805f0f
QuangDo <do.quang039@gmail.com>
2024-09-09 09:35:49 +0700

    test

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -7,11 +7,9 @@ export const defaultErrorHandler = (err: any, req: Request, res: Response, next:
   if (err instanceof ErrorWithStatus) {
     return res.status(err.status || HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json(omit(err, ['status']))
   }
-
   Object.getOwnPropertyNames(err).forEach((key) => {
     Object.defineProperty(err, key, { enumerable: true })
   })
-
   return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json({
     message: err.message,
     errorInfor: err
